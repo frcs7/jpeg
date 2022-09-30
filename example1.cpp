119e093867ae8cac7ff143d839a16902ea96a0f7
黄面条 <huangwz@zju.edu.cn>
2022-09-30 15:01:18 +0800

    final commit

--- example1.cpp
+++ example1.cpp
@@ -70,12 +70,182 @@ string binary(int a)//这是转换成游程编码所需的伪二进制
     }
 
     while (r.length() < m)
+
     {
+
       r = "0" + r;
+
     }
     
 
     return r;
+
+}
+
+void tomatrix(double *X, double (*matrixX)[256])//把Y通道数据写成矩阵
+
+{
+
+       for (int i = 0; i < 256; i++)
+
+   {
+
+      for (int j = 0; j < 256; j++)
+
+      {
+
+         matrixX[i][j] = X[256 * i + j] - 128; // Y数据的矩阵
+
+      }
+
+   }
+
+}
+
+void dividematrix(double (*matrixX)[256], double (*dmatrixX)[8][8])
+
+{
+
+   for (int i = 0; i < 256; i++)
+
+   {
+
+      for (int j = 0; j < 256; j++)
+
+      {
+
+         dmatrixX[32 * (i / 8) + j / 8][i % 8][j % 8] = matrixX[i][j]; //把Y数据分成32*32个小块
+
+      }
+
+   }
+
+}
+
+void DCTgeneral(double (*dmatrixX)[8][8])//对8*8矩阵做DCT变换
+
+{
+
+    double temp[8][8];
+
+   for (int k = 0; k < 32 * 32; k++)
+
+   {
+
+      for (int i = 0; i < 8; i++)
+
+      {
+
+         for (int j = 0; j < 8; j++)
+
+         {                                                     
+         
+            temp[i][j] = dmatrixX[k][i][j];              
+            
+
+         }
+
+      }
+
+      DCT(temp);
+
+      for (int i = 0; i < 8; i++)
+
+      {
+
+         for (int j = 0; j < 8; j++)
+
+         {                                                     
+         
+            dmatrixX[k][i][j] = temp[i][j];    
+            
+         }
+
+      }
+
+   }
+
+}
+
+void quantize_matrix(double (*dmatrixX)[8][8], short (*QdmatrixX)[8][8], short (*QX)[8])
+
+{
+
+          for (int k = 0; k < 32*32; k++)
+
+      {
+
+         for (int i = 0; i < 8; i++)
+
+         {
+
+            for (int j = 0; j < 8; j++)
+
+            {
+
+               QdmatrixX[k][i][j] = dmatrixX[k][i][j] / QX[i][j];
+
+            }
+
+         }
+
+      }
+
+}
+
+void zigzagX(double (*ZX)[64], short (*QdmatrixX)[8][8])
+
+{
+
+    for (int k = 0; k < 32*32; k++)
+
+   {
+
+      short rowY = 0, columnY = 1;
+
+      ZX[k][0] = QdmatrixX[k][0][0];
+
+      for (int i = 1; i <= 63; i++)
+
+      {
+
+         ZX[k][i] = QdmatrixX[k][rowY][columnY];
+
+         if ((rowY + columnY)%2 == 1)
+
+         {
+
+            rowY ++;
+
+            if (columnY != 0)
+
+            {
+
+               columnY --;
+
+            }
+
+         }
+
+         if ((rowY + columnY)%2 == 0)
+
+         {
+
+            columnY ++;
+
+            if (rowY != 0)
+
+            {
+
+               rowY --;
+
+            }
+
+         }
+
+      }
+
+   }
 
 }
 
@@ -167,207 +337,35 @@ int main()
 
    double matrixY[256][256];
 
-   //matrixY = tomatrix(Y);
+   tomatrix(Y,matrixY);//把Y通道数据写成矩阵
 
-
-   
-   for (int i = 0; i < 256; i++)
-
-   {
-
-      for (int j = 0; j < 256; j++)
-
-      {
-
-         //matrixY[i][j] = Y[256 * i + j] - 128; // Y数据的矩阵
-         matrixY[i][j] = Y[256 * i + j] - 128; // Y数据的矩阵
-      }
-   }
-
-  
    double dmatrixY[32 * 32][8][8];
 
-   for (int i = 0; i < 256; i++)
-
-   {
-
-      for (int j = 0; j < 256; j++)
-
-      {
-
-         dmatrixY[32 * (i / 8) + j / 8][i % 8][j % 8] = matrixY[i][j]; //把Y数据分成32*32个小块
-      }
-   }
+   dividematrix(matrixY,dmatrixY);//把Y矩阵分成32*32个小块
    
-   double temp[8][8];
-
-   for (int k = 0; k < 32 * 32; k++)
-
-   {
-
-      for (int i = 0; i < 8; i++)
-
-      {
-
-         for (int j = 0; j < 8; j++)
-
-         {                                                     
-         
-            temp[i][j] = dmatrixY[k][i][j];              
-            
-
-         }
-      }
-
-      DCT(temp);
-
-      for (int i = 0; i < 8; i++)
-
-      {
-
-         for (int j = 0; j < 8; j++)
-
-         {                                                     
-         
-            dmatrixY[k][i][j] = temp[i][j];                // Y通道的离散余弦变换完成啦！
-            
-
-         }
-      }
-
-   }
+   DCTgeneral(dmatrixY);//Y的离散余弦变换完成
 
 
    double matrixCb[256][256];
 
-   for (int i = 0; i < 256; i++)
-
-   {
-
-      for (int j = 0; j < 256; j++)
-
-      {
-
-         matrixCb[i][j] = Cb[256 * i + j] - 128; // Cb数据的矩阵
-      }
-   }
+   tomatrix(Cb,matrixCb);
 
    double dmatrixCb[32 * 32][8][8];
 
-   for (int i = 0; i < 256; i++)
-
-   {
-
-      for (int j = 0; j < 256; j++)
-
-      {
-
-         dmatrixCb[32 * (i / 8) + j / 8][i % 8][j % 8] = matrixCb[i][j]; //把Cb数据分成32*32个小块
-      }
-   }
+   dividematrix(matrixCb,dmatrixCb);
    
-
-   for (int k = 0; k < 32 * 32; k++)
-
-   {
-
-      for (int i = 0; i < 8; i++)
-
-      {
-
-         for (int j = 0; j < 8; j++)
-
-         {                                                     
-         
-            temp[i][j] = dmatrixCb[k][i][j];              
-            
-
-         }
-      }
-
-      DCT(temp);
-
-      for (int i = 0; i < 8; i++)
-
-      {
-
-         for (int j = 0; j < 8; j++)
-
-         {                                                     
-         
-            dmatrixCb[k][i][j] = temp[i][j];                // Cb通道的离散余弦变换完成啦！
-            
-
-         }
-      }
-
-   }
-
+   DCTgeneral(dmatrixCb);//Cb的离散余弦变换完成
 
    double matrixCr[256][256];
 
-   for (int i = 0; i < 256; i++)
-
-   {
-
-      for (int j = 0; j < 256; j++)
-
-      {
-
-         matrixCr[i][j] = Cr[256 * i + j] - 128; // Cr数据的矩阵
-      }
-   }
+   tomatrix(Cr,matrixCr);
 
    double dmatrixCr[32 * 32][8][8];
 
-   for (int i = 0; i < 256; i++)
-
-   {
-
-      for (int j = 0; j < 256; j++)
-
-      {
-
-         dmatrixCr[32 * (i / 8) + j / 8][i % 8][j % 8] = matrixCr[i][j]; //把Cr数据分成32*32个小块
-      }
-   }
+   dividematrix(matrixCr,dmatrixCr);
    
+   DCTgeneral(dmatrixCr);//Cr的离散余弦变换完成
 
-   for (int k = 0; k < 32 * 32; k++)
-
-   {
-
-      for (int i = 0; i < 8; i++)
-
-      {
-
-         for (int j = 0; j < 8; j++)
-
-         {                                                     
-         
-            temp[i][j] = dmatrixCr[k][i][j];              
-            
-
-         }
-      }
-
-      DCT(temp);
-
-      for (int i = 0; i < 8; i++)
-
-      {
-
-         for (int j = 0; j < 8; j++)
-
-         {                                                     
-         
-            dmatrixCr[k][i][j] = temp[i][j];                // Cr通道的离散余弦变换完成啦！
-            
-
-         }
-      }
-
-   }
 //三个通道的离散余弦变换全部完成！
 
       short QY[8][8] =
@@ -399,236 +397,35 @@ int main()
          {99,99,99,99,99,99,99,99}
 
       };
-//导入量化矩阵
+
+//导入量化矩阵void quantize_divided_matrix(double (*dmatrixX)[8][8], double (*QmatrixX)[8][8])
 
       short QdmatrixY[32*32][8][8];
 
-      for (int k = 0; k < 32*32; k++)
-
-      {
-
-         for (int i = 0; i < 8; i++)
-
-         {
-
-            for (int j = 0; j < 8; j++)
-
-            {
-
-               QdmatrixY[k][i][j] = dmatrixY[k][i][j] / QY[i][j];
-
-            }
-
-         }
-
-      }
-//dmatrixY矩阵量化处理完成！
+      quantize_matrix(dmatrixY, QdmatrixY, QY);//dmatrixY矩阵量化处理完成！
 
       short QdmatrixCr[32*32][8][8];
 
-      for (int k = 0; k < 32*32; k++)
-
-      {
-
-         for (int i = 0; i < 8; i++)
-
-         {
-
-            for (int j = 0; j < 8; j++)
-
-            {
-
-               QdmatrixCr[k][i][j] = dmatrixCr[k][i][j] / QC[i][j];
-
-            }
-
-         }
-
-      }
-//dmatrixCr矩阵量化处理完成！
+      quantize_matrix(dmatrixCr, QdmatrixCr, QC);//dmatrixCr矩阵量化处理完成！
 
       short QdmatrixCb[32*32][8][8];
 
-      for (int k = 0; k < 32*32; k++)
+      quantize_matrix(dmatrixCb, QdmatrixCb, QC);//dmatrixCb矩阵量化处理完成！
 
-      {
-
-         for (int i = 0; i < 8; i++)
-
-         {
-
-            for (int j = 0; j < 8; j++)
-
-            {
-
-               QdmatrixCb[k][i][j] = dmatrixCb[k][i][j] / QC[i][j];
-
-            }
-
-         }
-
-      }
-//dmatrixCb矩阵量化处理完成！
-//量化处理完成！
+//量化处理完成！void zigzagX(double (*ZY)[64], short (*Qdmatrix)[8][8])
 
    double ZY[32 * 32][64];
 
-   for (int k = 0; k < 32*32; k++)
-
-   {
-
-      short rowY = 0, columnY = 1;
-
-      ZY[k][0] = QdmatrixY[k][0][0];
-
-      for (int i = 1; i <= 63; i++)
-
-      {
-
-         ZY[k][i] = QdmatrixY[k][rowY][columnY];
-
-         if ((rowY + columnY)%2 == 1)
-
-         {
-
-            rowY ++;
-
-            if (columnY != 0)
-
-            {
-
-               columnY --;
-
-            }
-
-         }
-
-         if ((rowY + columnY)%2 == 0)
-
-         {
-
-            columnY ++;
-
-            if (rowY != 0)
-
-            {
-
-               rowY --;
-
-            }
-
-         }
-
-      }
-
-   }
-//量化处理后的Y数据矩阵ZigZag转换完成！
+   zigzagX(ZY, QdmatrixY);//量化处理后的Y数据矩阵ZigZag转换完成！
 
    double ZCr[32 * 32][64];
 
-   for (int k = 0; k < 32*32; k++)
-
-   {
-
-      short rowCr = 0, columnCr = 1;
-
-      ZCr[k][0] = QdmatrixCr[k][0][0];
-
-      for (int i = 1; i <= 63; i++)
-
-      {
-
-         ZCr[k][i] = QdmatrixCr[k][rowCr][columnCr];
-
-         if ((rowCr + columnCr)%2 == 1)
-
-         {
-
-            rowCr ++;
-
-            if (columnCr != 0)
-
-            {
-
-               columnCr --;
-
-            }
-
-         }
-
-         if ((rowCr + columnCr)%2 == 0)
-
-         {
-
-            columnCr ++;
-
-            if (rowCr != 0)
-
-            {
-
-               rowCr --;
-
-            }
-
-         }
-
-      }
-
-   }
-//量化处理后的Cr数据矩阵ZigZag转换完成！
+   zigzagX(ZCr, QdmatrixCr);//量化处理后的Cr数据矩阵ZigZag转换完成！
 
    double ZCb[32 * 32][64];
 
-   for (int k = 0; k < 32*32; k++)
+   zigzagX(ZCb, QdmatrixCb);//量化处理后的Cb数据矩阵ZigZag转换完成！
 
-   {
-
-      short rowCb = 0, columnCb = 1;
-
-      ZCb[k][0] = QdmatrixCb[k][0][0];
-
-      for (int i = 1; i <= 64; i++)
-
-      {
-
-         ZCb[k][i] = QdmatrixCb[k][rowCb][columnCb];
-
-         if ((rowCb + columnCb)%2 == 1)
-
-         {
-
-            rowCb ++;
-
-            if (columnCb != 0)
-
-            {
-
-               columnCb --;
-
-            }
-
-         }
-
-         if ((rowCb + columnCb)%2 == 0)
-
-         {
-
-            columnCb ++;
-
-            if (rowCb != 0)
-
-            {
-
-               rowCb --;
-
-            }
-
-         }
-
-      }
-
-   }
-//量化处理后的Cb数据矩阵ZigZag转换完成！
 //所有数据ZigZag转换完成！
 
    for (short k = 0; k < 32*32-1; k++)
@@ -654,14 +451,14 @@ int main()
       ZCb[32*32-1 - k][0] -=ZCb[32*32-2 - k][0];//对一维矩阵ZCb做差分
 
    }
+   
 //差分处理完成
-
-   
-   
 
    vector<vector<short> > YRLE;
 
    vector<short> YRLEtemp;
+
+   map<short, vector<vector<short> > > YRLEmap;//第一个short只是index
 
    for(short k = 0; k < 32*32; k++)
 
@@ -705,8 +502,6 @@ int main()
 
       }
 
-      map<short, vector<vector<short> > > YRLEmap;//第一个short只是index
-
       YRLEmap.insert(pair<short, vector<vector<short> > >(k, YRLE));
 
    }//Y通道游程编码完成
@@ -714,6 +509,8 @@ int main()
    vector<vector<short> > CrRLE;
 
    vector<short> CrRLEtemp;
+
+   map<short, vector<vector<short> > > CrRLEmap;//第一个short只是index
 
    for(short k = 0; k < 32*32; k++)
 
@@ -757,8 +554,6 @@ int main()
 
       }
 
-      map<short, vector<vector<short> > > CrRLEmap;//第一个short只是index
-
       CrRLEmap.insert(pair<short, vector<vector<short> > >(k, CrRLE));
 
    }//Cr通道游程编码完成
@@ -768,6 +563,8 @@ int main()
    vector<vector<short> > CbRLE;
 
    vector<short> CbRLEtemp;
+
+   map<short, vector<vector<short> > > CbRLEmap;//第一个short只是index
 
    for(short k = 0; k < 32*32; k++)
 
@@ -811,44 +608,10 @@ int main()
 
       }
 
-      map<short, vector<vector<short> > > CbRLEmap;//第一个short只是index
-
       CbRLEmap.insert(pair<short, vector<vector<short> > >(k, CbRLE));
 
    }   //Cb通道游程编码完成
 //游程编码全部完成，事实上所需的转换也已经完成，只需要用binary和codelength函数把RLE中的需变换的数字变换之后就可以了
-
-
-
-
-//下面是测试程序！
-   cout << ZY[1][0] << endl;
-
-   for (int i =0; i < 64; i++)
-
-   {
-
-      cout << ZY[0][i] << '\t';
-
-   }
-   cout << "\n" << endl;
-std::cerr <<  clock()/double(CLOCKS_PER_SEC) << std::endl;
-
-/*
-   for (int u = 0; u < NUM; u++)
-
-   {
-
-      for (int v = 0; v < NUM; v++)
-
-      {
-
-         cout << QdmatrixY[1][u][v] << '\t';                              //这里是测试程序！
-      }
-
-      cout << endl;
-   }
-*/
 
    return 0;
 }
